7ce56c9388ddd72543881da0cebce1799be221cc
Alumno <alumno@curso.es>
2026-03-06 20:27:42 +0100

    feat: migrate app to FastAPI

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def main():
-    print("Hello from git-tutorial for Digitech!")
+from fastapi import FastAPI
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+@app.get('/')
+def root():
+    return {'message': 'Hola mundo!'}
