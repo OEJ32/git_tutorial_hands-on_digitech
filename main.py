5bfa10209d85716865e89b75d782562e322682c9
Alumno <alumno@curso.es>
2026-03-06 20:27:39 +0100

    feat: add README and initial main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from fastapi import FastAPI
+def main():
+    print("Hello from git-tutorial for Digitech!")
 
-app = FastAPI()
-
-@app.get('/')
-def root():
-    return {'message': 'Hola mundo!'}
+if __name__ == "__main__":
+    main()
